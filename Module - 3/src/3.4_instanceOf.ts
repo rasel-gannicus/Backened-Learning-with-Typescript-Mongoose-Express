3b46a3d8bcad620857979f34765404fe453e5b1f
shafik720 <shafikrasel5@gmail.com>
2023-11-09 00:19:41 +0600

    learned : polymorphism, abstract, inheritence, encapsulation

--- Module - 3/src/3.4_instanceOf.ts
+++ Module - 3/src/3.4_instanceOf.ts
@@ -1,5 +1,5 @@
 
-
+{
 class Car {
     brand : string;
     color : string;
@@ -51,5 +51,67 @@ getCar(myCar1); // audi has six rings
 getCar(myCar2); // mercedez is named after founder's daughter
 getCar(new Car('toyota', 'white', 'japan')) // toyota is my car from japan in white color
 
-// myCar1.featureForAudi();
-// myCar2.featureForMercedez();
+}
+
+ //-- or we can write this code in this way 
+
+{
+    class Car {
+        brand : string;
+        color : string;
+        country : string; 
+    
+        constructor(brand:string, color:string, country:string){
+            this.brand = brand ; 
+            this.color = color ;
+            this.country = country ; 
+        }
+    
+        myCar(){
+            console.log(`${this.brand} is my car from ${this.country} in ${this.color} color`);
+        }
+    }
+    
+    class Audi extends Car {
+        constructor(brand:string, color:string, country:string){
+            super(brand, color, country) ; 
+        }
+        featureForAudi(){
+            console.log(`${this.brand} has six rings`);
+        }
+    }
+    
+    class Mercedez extends Car { 
+        constructor(brand:string, color:string, country:string){
+            super(brand, color, country) ; 
+        }
+        featureForMercedez(){
+            console.log(`${this.brand} is named after founder's daughter`);
+        }
+    }
+    
+    const isAudi = (car : Car) : car is Audi => {
+        return car instanceof Audi; 
+    }
+    const isMercedez = (car : Car) : car is Mercedez => {
+        return car instanceof Mercedez;
+    }
+
+    const getCar = (car : Car) => {
+        if(isAudi(car)){
+            car.featureForAudi();
+        }else if(isMercedez(car)){
+            car.featureForMercedez();
+        }else{
+            car.myCar();
+        }
+    }
+    
+    const myCar1 = new Audi('audi', 'white', 'england');
+    const myCar2 = new Mercedez('mercedez', 'silver', 'canada');
+    
+    getCar(myCar1); // audi has six rings
+    getCar(myCar2); // mercedez is named after founder's daughter
+    getCar(new Car('toyota', 'white', 'japan')) // toyota is my car from japan in white color
+    
+    }
