336ae80a2b0ab68f496644533f5a1ace3ad44054
shafik720 <shafikrasel5@gmail.com>
2023-11-22 18:38:42 +0600

    Mongoose core concept : configured eslint , prettier

--- Mongoose/Mastering the Core Concept of Mongoose/src/app/server.ts
+++ Mongoose/Mastering the Core Concept of Mongoose/src/app/server.ts
@@ -1,20 +1,17 @@
-import app from "./app";
-import config from "./config";
-import mongoose from "mongoose";
-
+import app from './app';
+import config from './config';
+import mongoose from 'mongoose';
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log(err);
-    }
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-
-
 
 main();
