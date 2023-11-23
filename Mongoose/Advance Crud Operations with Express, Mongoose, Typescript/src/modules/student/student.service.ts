8ea87a681ac364f72dc470820997ddab6ec8a184
shafik720 <shafikrasel5@gmail.com>
2023-11-24 02:51:52 +0600

    [Advance Crud Operations with Express, Mongoose, Typescript] : Started

---
+++ Mongoose/Advance Crud Operations with Express, Mongoose, Typescript/src/modules/student/student.service.ts
@@ -0,0 +1,26 @@
+import { Student } from './student.interface';
+import { StudentModel } from './student.model';
+
+// --- create a student's data
+const createStudentIntoDB = async (student: Student) => {
+  const result = await StudentModel.create(student);
+  return result;
+};
+
+// --- get all student data
+const getAllStudentsFromDB = async() => {
+    const result = await StudentModel.find();
+    return result ; 
+}
+
+// --- get single student data
+const getSingleStudent = async(studentId : string) => {
+    const result = await StudentModel.findOne({id : studentId}) ; 
+    return result ; 
+}
+
+export const StudentServices = {
+  createStudentIntoDB,
+  getAllStudentsFromDB,
+  getSingleStudent
+};
