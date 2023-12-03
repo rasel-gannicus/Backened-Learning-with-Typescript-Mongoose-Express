d9994ac525834df4a1bc10f372bc09734c5fcb3b
shafik720 <shafikrasel5@gmail.com>
2023-12-03 19:55:17 +0600

    creating a user

--- NoSQL Backened with Mongoose, Typescript/Advance Crud Operations with Express, Mongoose, Typescript/src/app/config/index.ts
+++ NoSQL Backened with Mongoose, Typescript/Advance Crud Operations with Express, Mongoose, Typescript/src/app/config/index.ts
@@ -6,4 +6,5 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  default_Password : process.env.DEFAULT_PASSWORD
 };
--- NoSQL Backened with Mongoose, Typescript/Advance Crud Operations with Express, Mongoose, Typescript/src/modules/user/user.services.ts
+++ NoSQL Backened with Mongoose, Typescript/Advance Crud Operations with Express, Mongoose, Typescript/src/modules/user/user.services.ts
@@ -1,8 +1,25 @@
+import config from "../../app/config";
+import { Student } from "../student/student.interface";
+import { NewUser } from "./user.interface";
+import { User } from "./user.model";
 
 
 // --- create a student's data
-const createStudentIntoDB = async (student: Student) => {
-  const result = await StudentModel.create(student);
+const createStudentIntoDB = async (password: string, studentData: Student) => {
+  const user : NewUser = {
+    password : password || (config.default_Password as string),
+    role : 'student' ,
+    id : '11111'
+  } ; 
+
+  // --- create a user
+  const result = await User.create(user);
+
+  // --- create a student after creating a user
+  if(Object.keys(user).length){
+    studentData.id = result.id ;
+    studentData.user = result._id ;
+  }
   return result;
 };
 
