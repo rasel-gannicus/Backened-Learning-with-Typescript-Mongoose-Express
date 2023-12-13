0772e93557f8e31fcfa51dad7cb04e5bd4a7f46d
shafik720 <shafikrasel5@gmail.com>
2023-12-13 22:08:46 +0600

    checking changes in branch 14.1

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,21 +2,40 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
+const globalErrorHandler : ErrorRequestHandler = (
+  err,
+  req,
+  res,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  type TErrorSource = {
+    path : string | number ;
+    message : string 
+  }[] ;
+
+  const errorSources : TErrorSource = [
+    {
+      path : '',
+      message : 'Something went wrong'
+    }
+  ]
+  if(err instanceof ZodError){
+    statusCode = 400  ; 
+    message = 'Ami zod error '
+  }
+
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources, 
+    amiError : err 
   });
 };
 
