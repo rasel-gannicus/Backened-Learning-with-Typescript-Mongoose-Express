8d74f65d7705527a1c253eecc5397a1c52c899f1
shafik720 <shafikrasel5@gmail.com>
2024-05-02 03:49:26 +0600

    creating layout with antd

---
+++ University Frontend Project with Typescript Redux/src/layout/MainLayout.tsx
@@ -0,0 +1,49 @@
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+
+
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0}} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
+
+export default MainLayout;
