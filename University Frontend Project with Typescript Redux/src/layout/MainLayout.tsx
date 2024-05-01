9b131c8a1b3b5ff2efdace427aaa0aabf03cf81d
shafik720 <shafikrasel5@gmail.com>
2024-05-02 03:58:04 +0600

    created main layout with ant design

--- University Frontend Project with Typescript Redux/src/layout/MainLayout.tsx
+++ University Frontend Project with Typescript Redux/src/layout/MainLayout.tsx
@@ -1,48 +1,58 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { createElement } from "react";
 
 
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  UserOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: createElement(icon),
+  label: `nav ${index + 1}`,
+}));
 
 const MainLayout = () => {
   return (
-    <div>
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
       <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="demo-logo-vertical" />
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={["4"]}
-            
-          />
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 0}} />
-          <Content style={{ margin: "24px 16px 0" }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-              }}
-            >
-              content
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-          </Footer>
-        </Layout>
+        <Header style={{ padding: 0, }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            Hello University
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
       </Layout>
-    </div>
+    </Layout>
   );
 };
 
